5d57d833df395761fc283e92f79cf922bb81c121
JssJssJssJ <159684986+JssJssJssJ@users.noreply.github.com>
2024-07-01 16:11:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const futureDate1 = new Date('2024-11-14');
     const timeDifference1 = futureDate1 - currentDate;
     const daysDifference1 = Math.ceil(timeDifference1 / (1000 * 60 * 60 * 24));
-    result1.textContent = `Syntymäpäivään enää ${daysDifference1} päivää!`;
+    result1.textContent = `Enää ${daysDifference1} päivää!`;
 
     const futureDate2 = new Date('2024-12-04');
     const timeDifference2 = futureDate2 - currentDate;
     const daysDifference2 = Math.ceil(timeDifference2 / (1000 * 60 * 60 * 24));
-    result2.textContent = `Syntymäpäivään enää ${daysDifference2} päivää!`;
+    result2.textContent = `Enää ${daysDifference2} päivää!`;
 });
