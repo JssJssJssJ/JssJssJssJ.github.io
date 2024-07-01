16df79147b5ea560a2532e32da36af4aad3c099f
JssJssJssJ <159684986+JssJssJssJ@users.noreply.github.com>
2024-07-01 16:02:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const result1 = document.getElementById('result1');
+    const result2 = document.getElementById('result2');
+
+    const currentDate = new Date();
+
+    const futureDate1 = new Date('2024-11-14');
+    const timeDifference1 = futureDate1 - currentDate;
+    const daysDifference1 = Math.ceil(timeDifference1 / (1000 * 60 * 60 * 24));
+    result1.textContent = `Syntymäpäivään enää ${daysDifference1} päivää!`;
+
+    const futureDate2 = new Date('2024-12-04');
+    const timeDifference2 = futureDate2 - currentDate;
+    const daysDifference2 = Math.ceil(timeDifference2 / (1000 * 60 * 60 * 24));
+    result2.textContent = `Syntymäpäivään enää ${daysDifference2} päivää!`;
+});
